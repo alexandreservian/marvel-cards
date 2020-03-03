dca840c68a667708607868e4914fea3489a80159
alexandreservian <alexandreservian@gmail.com>
2020-03-03 13:54:11 -0300

    O componente principal para a iniciação do mesmo, é o component App

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import RoutersApp from 'routes';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <RoutersApp />
+    </BrowserRouter>
+  );
+};
+
+export default App;
