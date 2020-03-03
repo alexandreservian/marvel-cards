d9646086f4e1ae17c361a2c3696e0ca300fb5e2d
alexandreservian <alexandreservian@gmail.com>
2020-03-03 13:59:43 -0300

    Aqui fica o componente que gerencia as rotas da nossa aplicação

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React, { Suspense, lazy } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import LoadingBar from 'components/loading-bar';
+
+const ListCards = lazy(() => import('pages/list-cards'));
+const Card = lazy(() => import('pages/card'));
+
+const RoutersApp = () => {
+  return (
+    <Suspense fallback={<LoadingBar />}>
+      <Switch>
+        <Redirect exact from="/" to="/card-list" />
+        <Route exact path="/card-list" component={ListCards} />
+        <Route exact path="/card-list/:id" component={Card} />
+      </Switch>
+    </Suspense>
+  );
+};
+
+export default RoutersApp;
