57846dd4f93837b3c9280984c94094c4cdd8df2b
alexandreservian <alexandreservian@gmail.com>
2020-03-04 11:04:24 -0300

    Foi adicionado ao App o provider theme, agora podemos resgatar todas as cores em qualquer componente

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'emotion-theming';
 import RoutersApp from 'routes';
 import store from 'state/store';
+import theme from 'themes';
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <RoutersApp />
+        <ThemeProvider theme={theme}>
+          <RoutersApp />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   );
