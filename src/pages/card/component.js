bd4a5b71fab2689908e7e2cd50ceae26e9611a81
alexandreservian <alexandreservian@gmail.com>
2020-03-05 20:41:05 -0300

    Começando a implementar a pagina interna do card

--- src/pages/card/component.js
+++ src/pages/card/component.js
@@ -1,6 +1,9 @@
 // @flow
 import React, { useEffect, memo } from 'react';
 import { useParams } from 'react-router-dom';
+import Header from 'components/header';
+import ButtonBackPage from 'components/button-back-page';
+import { Content } from './style';
 
 type Props = {
   card: Object<any>,
@@ -14,7 +17,13 @@ const Card = ({ card, searchCard }: Props): React.Node => {
     searchCard(id);
   }, [searchCard, id]);
 
-  return <div />;
+  return (
+    <Content>
+      <Header>
+        <ButtonBackPage />
+      </Header>
+    </Content>
+  );
 };
 
 export default memo(Card);
