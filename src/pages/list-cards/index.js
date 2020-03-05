2ace93c059d4fa78f5e15cc41929a3d270266b9e
alexandreservian <alexandreservian@gmail.com>
2020-03-05 15:48:50 -0300

    Refatorando o codigo para deixa-lo menor e de facil entendimento

--- src/pages/list-cards/index.js
+++ src/pages/list-cards/index.js
@@ -4,13 +4,15 @@ import { actions, selectors } from 'state/ducks/list-card';
 import ListCards from './component';
 
 const mapStateToProps = state => ({
-  cards: selectors.getAllListCards(state)
+  cards: selectors.getAllListCards(state),
+  totalPages: selectors.getTotalPages(state),
+  loading: selectors.getLoading(state)
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      searchCards: actions.searchCards
+      searchCards: actions.searchCards({ limit: 23 })
     },
     dispatch
   );
