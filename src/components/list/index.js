f3cdc8ac5451a36817c6b8b4c888b42ddf90d1b0
alexandreservian <alexandreservian@gmail.com>
2020-03-05 15:33:43 -0300

    Removi o List da list-cards, e criei um componente isolado só para a lista que receber os cards

---
+++ src/components/list/index.js
@@ -0,0 +1,21 @@
+//@flow
+import React, { memo } from 'react';
+import visibleComponentHoc from 'components/visible-component-hoc';
+import Card from 'components/card';
+import { Content } from './style';
+
+type Props = {
+  cards: Array<any>
+};
+
+const List = ({ cards }: Props): React.Node => {
+  return (
+    <Content>
+      {cards.map(({ id, name, thumbnail }) => (
+        <Card key={id} id={id} name={name} thumbnail={thumbnail} />
+      ))}
+    </Content>
+  );
+};
+
+export default memo(visibleComponentHoc(List));
---
+++ src/components/list/style.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const Content = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0 auto;
+  overflow: hidden;
+  width: 90%;
+
+  @media (min-width: 1201px) {
+    padding: 0 0 5% 0;
+  }
+
+  @media (max-width: 1200px) and (min-width: 901px) {
+    padding-bottom: 6%;
+  }
+
+  @media (max-width: 900px) and (min-width: 601px) {
+    padding-bottom: 7%;
+  }
+
+  @media (max-width: 600px) {
+    padding: 0 0 13% 0;
+  }
+`;
